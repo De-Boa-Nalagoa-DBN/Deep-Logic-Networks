e26e36a7658c9a58eb2efa6fb3ca08430a226a1b
soareswallace <wssj2@cin.ufpe.br>
2018-06-01 23:06:55 -0300

    initial commit INF1+quan inference

--- src/inf1.py
+++ src/inf1.py
@@ -1,1 +1,16 @@
-def inferenceRule(rule):
+def inferenceRule(hipothese, missing_rule, min, max):
+    sum_alfa_t = 0.0
+    sum_alfa_k = 0.0
+    alfa_m = (min+max)/2
+    confidenceValue = hipothese.c
+    for literal in hipothese.x:
+        if literal:
+            sum_alfa_t += 1
+        else:
+            sum_alfa_k += 1
+    alfa_h = confidenceValue*(sum_alfa_t-sum_alfa_k)
+    # TODO como lidar com missing values?
+    # é preciso saber quantos missing values TRUES e FALSES há
+    # na hipotese. Porem a hipotese contem valores
+    # trues e falses(em x), mas nao contem quais deles
+    # estao missing.
