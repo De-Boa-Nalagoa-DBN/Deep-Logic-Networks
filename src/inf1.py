4a30c936db26f4244339a4acb677d9d3b5b8c50e
soareswallace <wssj2@cin.ufpe.br>
2018-06-02 17:04:27 -0300

    Idea + New class call belief(I think we gonna need it

--- src/inf1.py
+++ src/inf1.py
@@ -1,16 +1,15 @@
-def inferenceRule(hipothese, missing_rule, min, max):
-    sum_alfa_t = 0.0
-    sum_alfa_k = 0.0
+
+
+def inferenceRule(rule, min, max, array_of_beliefs):
+    alfa_t = 0.0
+    alfa_k = 0.0
+    how_many_missing_trues = 0
+    how_many_missing_falses = 0
+    confidence_value = rule.c
     alfa_m = (min+max)/2
-    confidenceValue = hipothese.c
-    for literal in hipothese.x:
-        if literal:
-            sum_alfa_t += 1
+    #TODO nao sei como identificar o missing
+    for literals in rule.x:
+        if literals:
+            alfa_t += 1.0
         else:
-            sum_alfa_k += 1
-    alfa_h = confidenceValue*(sum_alfa_t-sum_alfa_k)
-    # TODO como lidar com missing values?
-    # é preciso saber quantos missing values TRUES e FALSES há
-    # na hipotese. Porem a hipotese contem valores
-    # trues e falses(em x), mas nao contem quais deles
-    # estao missing.
+            alfa_k += 1.0
