db2892eed72fd32a03babbdef62f2d56150529c5
Ítalo Paulino <irbp@cin.ufpe.br>
2018-06-03 16:52:13 -0300

    Ignoring backpropagation layer

--- src/modified_dbn.py
+++ src/modified_dbn.py
@@ -11,7 +11,7 @@ from modified_rbm import RBM2
 
 class DBN(object):
 
-    def __init__(self, sizes, X, Y, epochs=10, batch_size=100, learning_rate=1.0, momentum=0.0):
+    def __init__(self, sizes, X, Y, epochs=10, batch_size=100, learning_rate=1.0, momentum=0.0, with_rules=False):
         self._sizes = sizes
         self._X = X
         self._Y = Y
@@ -23,20 +23,36 @@ class DBN(object):
         self._momentum = momentum
         self._epochs = epochs
         self._batch_size = batch_size
+        self.with_rules = with_rules
         input_size = X.shape[1]
 
-        # initializing
-        for size in self._sizes + [self._Y.shape[1]]:
-            # Defining upper limit for the uniform distribution range
-            max_range = 4 * math.sqrt(6. / (input_size + size))
 
-            # Initialize weights through a random uniform distribution
-            self.w_list.append(
-                np.random.uniform(-max_range, max_range, [input_size, size]).astype(np.float32))
+        if self.with_rules:
+            # initializing
+            for size in self._sizes:
+                # Defining upper limit for the uniform distribution range
+                max_range = 4 * math.sqrt(6. / (input_size + size))
 
-            # Initialize bias with zeros
-            self.b_list.append(np.zeros([size], np.float32))
-            input_size = size
+                # Initialize weights through a random uniform distribution
+                self.w_list.append(
+                    np.random.uniform(-max_range, max_range, [input_size, size]).astype(np.float32))
+
+                # Initialize bias with zeros
+                self.b_list.append(np.zeros([size], np.float32))
+                input_size = size
+        else:
+            # initializing
+            for size in self._sizes + [self._Y.shape[1]]:
+                # Defining upper limit for the uniform distribution range
+                max_range = 4 * math.sqrt(6. / (input_size + size))
+
+                # Initialize weights through a random uniform distribution
+                self.w_list.append(
+                    np.random.uniform(-max_range, max_range, [input_size, size]).astype(np.float32))
+
+                # Initialize bias with zeros
+                self.b_list.append(np.zeros([size], np.float32))
+                input_size = size
 
     def train_rbms(self):
         inpX = self._X
@@ -86,21 +102,39 @@ class DBN(object):
 
     def train(self):
         # Create placeholders for input, weeights, biases and output
-        _in = [None] * (len(self._sizes) + 2)
-        _w = [None] * (len(self._sizes) + 1)
-        _b = [None] * (len(self._sizes) + 1)
-        _in[0] = tf.placeholder("float", [None, self._X.shape[1]])
-        y = tf.placeholder("float", [None, self._Y.shape[1]])
+        if self.with_rules:
+            _in = [None] * (len(self._sizes) + 1)
+            _w = [None] * (len(self._sizes))
+            _b = [None] * (len(self._sizes))
+            _in[0] = tf.placeholder("float", [None, self._X.shape[1]])
+            y = tf.placeholder("float", [None, self._Y.shape[1]])
 
-        # Initializing variables
-        for i in range(len(self._sizes) + 1):
-            _w[i] = tf.Variable(self.w_list[i])
-            _b[i] = tf.Variable(self.b_list[i])
+            # Initializing variables
+            for i in range(len(self._sizes)):
+                _w[i] = tf.Variable(self.w_list[i])
+                _b[i] = tf.Variable(self.b_list[i])
 
-        # Defining activation function
-        for i in range(1, len(self._sizes) + 2):
-            _in[i] = tf.nn.sigmoid(
-                tf.matmul(_in[i - 1], _w[i - 1]) + _b[i - 1])
+            # Defining activation function
+            for i in range(1, len(self._sizes) + 1):
+                _in[i] = tf.nn.sigmoid(
+                    tf.matmul(_in[i - 1], _w[i - 1]) + _b[i - 1])
+
+        else:
+            _in = [None] * (len(self._sizes) + 2)
+            _w = [None] * (len(self._sizes) + 1)
+            _b = [None] * (len(self._sizes) + 1)
+            _in[0] = tf.placeholder("float", [None, self._X.shape[1]])
+            y = tf.placeholder("float", [None, self._Y.shape[1]])
+
+            # Initializing variables
+            for i in range(len(self._sizes) + 1):
+                _w[i] = tf.Variable(self.w_list[i])
+                _b[i] = tf.Variable(self.b_list[i])
+
+            # Defining activation function
+            for i in range(1, len(self._sizes) + 2):
+                _in[i] = tf.nn.sigmoid(
+                    tf.matmul(_in[i - 1], _w[i - 1]) + _b[i - 1])
 
         # Define cost function
         cost = tf.reduce_mean(tf.square(_in[-1] - y))
@@ -121,10 +155,16 @@ class DBN(object):
                     sess.run(train_op, feed_dict={
                              _in[0]: self._X[start:end], y: self._Y[start:end]})
 
-                for j in range(len(self._sizes) + 1):
-                    # Updates weights and biases
-                    self.w_list[j] = sess.run(_w[j])
-                    self.b_list[j] = sess.run(_b[j])
+                if self.with_rules:
+                    for j in range(len(self._sizes)):
+                        # Updates weights and biases
+                        self.w_list[j] = sess.run(_w[j])
+                        self.b_list[j] = sess.run(_b[j])
+                else:
+                    for j in range(len(self._sizes) + 1):
+                        # Updates weights and biases
+                        self.w_list[j] = sess.run(_w[j])
+                        self.b_list[j] = sess.run(_b[j])
 
                 print("Accuracy rating for epoch " + str(i) + ": " + str(np.mean(np.argmax(self._Y, axis=1) ==
                                                                                  sess.run(predict_op, feed_dict={_in[0]: self._X, y: self._Y}))))
@@ -159,7 +199,7 @@ class DBN(object):
         input_size = inpX.shape[1]
 
         # for each RBM we want to generate
-        for i, size in enumerate(self._sizes):
+        for i, size in enumerate(self._sizes[:-1]):
             rbm = RBM2(input_size, size)
             rbm.insertKnowledge(knowledgeBase[i])
             rbm_list.append(rbm)
@@ -170,6 +210,11 @@ class DBN(object):
             print('\n\nRBM {}: '.format(i))
             rbm.train(inpX)
             inpX = rbm.rbm_output(inpX)
+
+        # fine tuning layer
+        rbm = RBM2(input_size, self._sizes[-1])
+        rbm.insertKnowledge(knowledgeBase[-1])
+        rbm_list.append(rbm)
 
         return rbm_list
 
