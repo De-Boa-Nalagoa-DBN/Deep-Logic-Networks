d881255df60b3a24083ba27a7b95d46d414269d4
Ítalo Paulino <irbp@cin.ufpe.br>
2018-06-10 20:56:15 -0300

    modifying some stuffs

--- src/inference.py
+++ src/inference.py
@@ -3,21 +3,19 @@ from rule import Rule
 import numpy as np
 
 def inf1(rule, input):
-    sum = 0
-    for i in range(len(rule.x)):
-        if rule.x[i] == True:
-            sum += input[i]
-        elif rule.x[i] == False:
-            sum -= input[i]
-    return sum*rule.c
+    positives = np.sum(input[rule.x == True])
+    negatives = np.sum(input[rule.x == False])
 
-
+    return (positives - negatives) * rule.c
 
 def quantitativeInference(ruleSet, input_data):
     current_data = input_data
     for _, rules in enumerate(ruleSet):
         alfas = [inf1(r, current_data) for r in rules]
-        alfas = alfas / np.linalg.norm(alfas, ord=np.inf, axis=0, keepdims=True)
+        alfas = np.asarray(alfas)
+        # print(alfas.shape)
+        alfas = alfas / np.max(alfas)
+        # alfas = alfas / np.linalg.norm(alfas, ord=np.inf, axis=0, keepdims=True)
         current_data = alfas
 
     return current_data
@@ -25,8 +23,8 @@ def quantitativeInference(ruleSet, input_data):
 def main():
     r = Rule(0)
     r.c = 1.5
-    r.x = [True, False, True]
-    input = [1, 0.5, 1]
+    r.x = np.array([True, False, True])
+    input = np.array([1, 0.5, 1])
     output = quantitativeInference([[r]], input)
     print(output)
 
